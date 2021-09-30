54dd6656ea1193b0e3c73aa0e04282947bb24fe5
Maxim Palyanov <maksimpalanov6245@gmail.com>
2021-09-30 23:30:30 +0300

    Add comment

--- Project_2/src/ru/Palyanaff/SamsungAcademy/Main.java
+++ Project_2/src/ru/Palyanaff/SamsungAcademy/Main.java
@@ -3,6 +3,6 @@ package ru.Palyanaff.SamsungAcademy;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World!"); // Output "Hello World!"
     }
 }
