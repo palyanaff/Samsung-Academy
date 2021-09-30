57bdd0473d529ea11a3b2626969e63899932e093
Maxim Palyanov <maksimpalanov6245@gmail.com>
2021-10-01 01:26:39 +0300

    Improve sum

--- Project_2/src/ru/Palyanaff/SamsungAcademy/Main.java
+++ Project_2/src/ru/Palyanaff/SamsungAcademy/Main.java
@@ -1,8 +1,23 @@
 package ru.Palyanaff.SamsungAcademy;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Output "Hello World!"
+        /*System.out.println("Hello World!"); // Output "Hello World!"*/
+        Scanner in = new Scanner(System.in);
+
+        int a, b, c;
+
+        /*System.out.println("Enter first number: ");*/
+        a = in.nextInt();
+
+        /*System.out.println("Enter second number: ");*/
+        b = in.nextInt();
+
+        c = a + b;
+
+        System.out.println("Sum = " + c);
     }
 }
