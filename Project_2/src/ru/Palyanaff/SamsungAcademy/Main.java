17232b868f2949c865f3722375347d725ff0ed1e
Maxim Palyanov <maksimpalanov6245@gmail.com>
2021-10-01 14:59:02 +0300

    Change Project_2

--- Project_2/src/ru/Palyanaff/SamsungAcademy/Main.java
+++ Project_2/src/ru/Palyanaff/SamsungAcademy/Main.java
@@ -5,19 +5,9 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        /*System.out.println("Hello World!"); // Output "Hello World!"*/
-        Scanner in = new Scanner(System.in);
-
-        int a, b, c;
-
-        /*System.out.println("Enter first number: ");*/
-        a = in.nextInt();
-
-        /*System.out.println("Enter second number: ");*/
-        b = in.nextInt();
-
-        c = a + b;
-
-        System.out.println("Sum = " + c);
+        int x = 1000 * 1000;
+        System.out.println((12.72) % 3);
     }
 }
+
+
