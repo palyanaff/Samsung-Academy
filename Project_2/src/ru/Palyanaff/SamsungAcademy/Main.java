541834a5f96b595e03a06888e4ab8c4e66806966
Maxim Palyanov <maksimpalanov6245@gmail.com>
2021-10-02 18:03:42 +0300

    Finish 1.2 task

--- Project_2/src/ru/Palyanaff/SamsungAcademy/Main.java
+++ Project_2/src/ru/Palyanaff/SamsungAcademy/Main.java
@@ -5,8 +5,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        int x = 1000 * 1000;
-        System.out.println((12.72) % 3);
+        int s, m, h, n;
+        Scanner in = new Scanner(System.in);
+
+        n = in.nextInt();
+
+        h = (n / 3600) % 24;
+
+        m = (n / 60) % 60;
+
+        s = n % 60;
+        if (m / 10 == 0 && s / 10 == 0)
+            System.out.println(h + ":0" + m + ":0" + s);
+        else if (m / 10 == 0 && s / 10 != 0)
+            System.out.println(h + ":0" + m + ":" + s);
+        else if (m / 10 != 0 && s / 10 == 0)
+            System.out.println(h + ":" + m + ":0" + s);
+        else
+            System.out.println(h + ":" + m + ":" + s);
     }
 }
 
