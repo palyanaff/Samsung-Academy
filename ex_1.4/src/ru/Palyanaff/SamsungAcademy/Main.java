7be2338e1a3cfb0a0c8e59b2b6a39018dc352985
Maxim Palyanov <maksimpalanov6245@gmail.com>
2021-10-05 22:31:44 +0300

    Finish ex_1.4

--- ex_1.4/src/ru/Palyanaff/SamsungAcademy/Main.java
+++ ex_1.4/src/ru/Palyanaff/SamsungAcademy/Main.java
@@ -1,8 +1,46 @@
 package ru.Palyanaff.SamsungAcademy;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner in = new Scanner(System.in);
+        int A;
+        boolean f = true;
+
+        A = in.nextInt();
+
+        if (A == 1){
+            f = true;
+        }
+        else{
+            while(A != 0)
+            {
+                if (A == 1){
+                    //f = false;
+                    break;
+                }
+                else if (A % 2 != 0){
+                    f = false;
+                    break;
+                }
+                A = A / 2;
+            }
+        }
+
+        if (f){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+
+        //A = A & (1 << i) - 1;
+        //A = A ^ (1 << 1) << i - 1;
+        //A = A | (1 << 1) << i - 1;
+        //int i1 = i != 0 ? A & -(1 << i) : A;
+
+        //System.out.println(A);
     }
 }
